2740789a517add4bc0389c0f285343b034ae34ea
zhaojintian <729234283@qq.com>
2018-10-08 09:19:21 +0800

    update

--- src/utils/consts/index.js
+++ src/utils/consts/index.js
@@ -4,7 +4,7 @@ import MAPS from './maps'
 
 // 基础地址
 // const BASE_URL = env.isDev() ? 'http://localhost:8080' : 'http://hd.xingxiaoban.com:8080'
-const BASE_URL = 'http://hd.xingxiaoban.com:8080'
+const BASE_URL = 'https://acs-api.liruan.cn'
 
 // 接口地址
 const API_URL = BASE_URL
